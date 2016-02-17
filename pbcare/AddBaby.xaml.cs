c19e5155e0db35ee40da38f527365746bb10ddfa
SaudMakeen <s.makeen@hotmail.com>
2016-02-17 21:55:13 +0300

    Baby care - Add baby button with info

---
+++ pbcare/AddBaby.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace pbcare
+{
+	public partial class AddBaby : ContentPage
+	{
+		public AddBaby ()
+		{
+			InitializeComponent ();
+		}
+
+		public void saveChildInfo(object sender , EventArgs e){
+			var child = new Child ();
+			child.name = childName.Text;
+			//child.birthDate = birthdate;
+			child.gender = "m";
+			s.Text = child.name +" "+child.gender; 
+			s.TextColor = Color.Red;
+		}
+
+		public void saveChildCancled(object sender , EventArgs e){
+			Navigation.PopAsync ();
+		}
+
+	}
+}
+
---
+++ pbcare/Child.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace pbcare
+{
+	public class Child 
+	{
+		public Image ChildImage { get; set; }
+		public string name { get; set; }
+		//public DatePicker birthDate { get; set; }
+		public string gender { get; set;}
+	}
+
+}
+
+
